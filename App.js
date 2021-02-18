de54c1c6c1f730340ec5223eadd21061ead97ff4
Chee Kit <cheekit.chong98@gmail.com>
2021-02-18 09:18:24 +0800

    Fixed status bar height

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, StatusBar } from "react-native";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
@@ -8,7 +9,9 @@ export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
-      <AppNavigator />
+      <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <AppNavigator />
+      </View>
     </ApplicationProvider>
   </>
 );
