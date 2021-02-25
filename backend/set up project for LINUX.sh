561b7cdc9d2af95313d488acbcda08247af91a9d
beazt123 <lohzhixuan@live.com>
2021-02-25 20:46:53 +0800

    Added scripts to automate Flask set up and testing for linux and windows

---
+++ backend/set up project for LINUX.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv 
+
+virtualenv -p python3 backend
+source backend/bin/activate
+pip install Flask
+pip install Flask-PyMongo
+pip install Flask-Login
+
