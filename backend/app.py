51f5325ff6124a249628088f1f35001e6df585d6
beazt123 <lohzhixuan@live.com>
2021-03-03 18:15:51 +0800

    Added /audits endpoint with the POST function to the main app

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from BTS.auditsEndpoint import processAuditdata
+from BTS.utils import successMsg
+from flask import Flask, request
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/singhealth"
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, good World!'
+
+
+@app.route("/audits", methods = ['POST'])
+def audits():
+    if request.method == 'POST':
+        auditData = request.json
+        filledAuditForms , auditMetaData = processAuditdata(auditData)
+
+        mongo.db.audits.insert_one(auditMetaData)
+        mongo.db.filledAuditForms.insert_one(filledAuditForms)
+
+        return successMsg("Forms have been submitted"), 200
