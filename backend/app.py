e7be26d1c2193f23ada140ea9eaf0c5e553d88fc
beazt123 <lohzhixuan@live.com>
2021-03-03 19:02:30 +0800

    Disallow uploading of duplicate forms. Server will abort and reply with a error 400

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from BTS.auditsEndpoint import processAuditdata
-from BTS.utils import successMsg
+from BTS.utils import successMsg, failureMsg
+from pymongo.errors import DuplicateKeyError
 from flask import Flask, request
 from flask_pymongo import PyMongo
 
@@ -19,7 +20,12 @@ def audits():
         auditData = request.json
         filledAuditForms , auditMetaData = processAuditdata(auditData)
 
-        mongo.db.audits.insert_one(auditMetaData)
-        mongo.db.filledAuditForms.insert_one(filledAuditForms)
+
+        try:
+            mongo.db.audits.insert_one(auditMetaData)
+            for filledForm in filledAuditForms.values():
+                mongo.db.filledAuditForms.insert_one(filledForm)
+        except DuplicateKeyError:
+            return failureMsg("Form has already been uploaded", 400), 400
 
         return successMsg("Forms have been submitted"), 200
