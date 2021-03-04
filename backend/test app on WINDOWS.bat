1155c6ec9fed894f21f4b12bf2313dfecfd1ad3e
beazt123 <lohzhixuan@live.com>
2021-03-05 01:50:14 +0800

    Migrated from pip to pipenv

--- backend/test app on WINDOWS.bat
+++
@@ -1,9 +0,0 @@
-copy /y app.py backend\Lib\site-packages
-xcopy BTS backend\Lib\site-packages\BTS /s/e/y/d/i
-
-
-
-echo Running app.py
-call backend\Scripts\activate
-set FLASK_APP=app.py
-flask run
