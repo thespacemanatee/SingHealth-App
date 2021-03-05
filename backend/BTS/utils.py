4cd92e7f3024839bd963c92e7c8ca4726f4e7911
beazt123 <lohzhixuan@live.com>
2021-03-05 14:18:40 +0800

    Added an /image endpoint with POST method. Only works for uploading in buffered Readers
    Added the /images endpoint with POST method. Only supports uploading a buffered reader, with no support for base64

--- backend/BTS/utils.py
+++ backend/BTS/utils.py
@@ -1,3 +1,42 @@
+import io
+import boto3
+from PIL import Image
+
+
+def upload_image(file_obj, bucket, file_name):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_fileobj(file_obj, bucket, file_name)
+    return response
+
+
+def download_image(file_name, bucket):
+    """
+    Function to download a given file from an S3 bucket
+    """
+    s3 = boto3.resource('s3')
+    file_stream = io.BytesIO()
+    s3.Bucket(bucket).download_fileobj(file_name, file_stream)
+
+    return file_stream
+
+
+def list_images(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3 = boto3.client('s3')
+    contents = []
+    # try:
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+    # except Exception as e:
+    #     pass
+
+    return contents
+
 def successMsg(msg):
     output = dict()
     output["status"] = 200
@@ -9,3 +48,10 @@ def failureMsg(msg, code):
     output["status"] = code
     output["description"] = msg
     return output
+
+
+if __name__ == "__main__":
+    print("helllo world")
+    with open("bullseye2.png", "rb") as f:
+        upload_image(f, "singhealth", "bullseye2.png")
+    print("done")
--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from BTS.auditsEndpoint import processAuditdata
-from BTS.utils import successMsg, failureMsg
+from BTS.utils import successMsg, failureMsg, upload_image, download_image
 from pymongo.errors import DuplicateKeyError
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/singhealth"
@@ -29,5 +30,21 @@ def audits():
             return failureMsg("Form has already been uploaded", 400), 400
 
         return successMsg("Forms have been submitted"), 200
+
+@app.route("/images", methods = ["GET", 'POST'])
+def images():
+    if request.method == 'POST':
+        formdata = request.files
+        images = formdata.getlist("images") 
+        
+        for image in images:
+            imgName = image.filename
+            upload_image(image, "singhealth", imgName)
+        
+        return successMsg("Pictures have successfully been uploaded"), 200
+        
+
+
+
 
 app.run(debug=True)
