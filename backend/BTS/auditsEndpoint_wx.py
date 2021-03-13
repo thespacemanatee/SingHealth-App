08186aa6d70a403ed5732b86494f3c4cf72f4ad8
beazt123 <lohzhixuan@live.com>
2021-03-14 01:47:17 +0800

    Joined GET /tenants and GET /auditForms to the main app

--- backend/BTS/auditsEndpoint_wx.py
+++ backend/BTS/auditsEndpoint_wx.py
@@ -5,98 +5,86 @@ Created on Fri Mar  5 02:46:03 2021
 @author:wx
 """
 
-from flask import Flask, render_template, jsonify
-from flask_pymongo import PyMongo
-from bson import json_util, ObjectId
-from bson.json_util import dumps, loads
+from flask import jsonify, make_response
 import json
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/BTS"
-mongo = PyMongo(app)
+def addWenXinEndpoints(app, mongo):
+    #Able to retrieve tenant and audit form information and return as json string
+    @app.route("/tenants/<institutionID>", methods = ["GET"])
+    def get_tenants_from_institution(institutionID): 
+        try:
+            tenants = mongo.db.tenant.find({"institutionID": institutionID})
 
-#Able to retrieve tenant and audit form information and return as json string
-@app.route("/tenants/<institutionID>", methods = ["GET"])
-def get_tenants_from_institution(institutionID): 
-    try:
-        tenants = mongo.db.tenant.find({"institutionID": institutionID})
-
-        result = [{
-            'tenantID' : tenant['_id'], 
-            'stallName' : tenant["stall"]["name"]
-            }
-                  for tenant in tenants]
-        
-        #if data is found
-        if len(result) > 0 :
-            output = {
-                "status" : 200,
-                "description" : "success",
-                "data" : result}
-        else:
-            output = {
-                "status" : 200,
-                "description" : "no matching data",
-                "data" : []}
+            result = [{
+                'tenantID' : tenant['_id'], 
+                'stallName' : tenant["stall"]["name"]
+                }
+                    for tenant in tenants]
             
-        
-    except:
-        output = {
-                "status" : 404,
-                "description" : "error in connection",
-                "data" : []}
-    
-    json_data = dumps(output)
-    return json_data
-
-@app.route("/auditForms/<form_type>", methods = ["GET"])
-def get_audit_form(form_type):
-    
-    try:
-        form = mongo.db.auditFormTemplate.find_one(
-            {"type": form_type, "currentForm": True})  
-        
-        checklist = []
-        if form is not None:
-            #access the questions with each categories
-            for key, value in form["categories"].items():
-                value = {
-                    "category" : value,
-                    "questions" : form[key]["questions"]
-                    }
-                checklist.append(value)
+            #if data is found
+            if len(result) > 0 :
+                output = {
+                    "status" : 200,
+                    "description" : "success",
+                    "data" : result}
+            else:
+                output = {
+                    "status" : 200,
+                    "description" : "no matching data",
+                    "data" : []}
                 
+            
+        except:
             output = {
-                "status" : 200,
-                "description" : "success",
-                "data" : {
-                    "formID" : form["_id"],
-                    "type" : form["type"],
-                    "checklist" : checklist
+                    "status" : 404,
+                    "description" : "error in connection",
+                    "data" : []}
+        
+        response = make_response(jsonify(output), output['status'])
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
+
+    @app.route("/auditForms/<form_type>", methods = ["GET"])
+    def get_audit_form(form_type):
+        
+        try:
+            form = mongo.db.auditFormTemplate.find_one(
+                {"type": form_type, "currentForm": True})  
+            
+            checklist = []
+            if form is not None:
+                #access the questions with each categories
+                for key, value in form["categories"].items():
+                    value = {
+                        "category" : value,
+                        "questions" : form[key]["questions"]
+                        }
+                    checklist.append(value)
+                    
+                output = {
+                    "status" : 200,
+                    "description" : "success",
+                    "data" : {
+                        "formID" : form["_id"],
+                        "type" : form["type"],
+                        "checklist" : checklist
+                        }
                     }
-                }
-            
-        else:
+                
+            else:
+                output = {
+                    "status" : 200,
+                    "description" : "no matching form",
+                    "data" : {}
+                    }
+                
+        except:
             output = {
-                "status" : 200,
-                "description" : "no matching form",
-                "data" : {}
-                }
-            
-    except:
-         output = {
-                "status" : 404,
-                "description" : "unspecified connection/data error",
-                "data" : []}
-    
-    json_data = dumps(output)
-    return json_data
+                    "status" : 404,
+                    "description" : "unspecified connection/data error",
+                    "data" : []}
+        
+        response = make_response(jsonify(output), output['status'])
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
 
-#Flask test
-@app.route('/')
-def hello_world():
-    online_users = mongo.db.institution.find({"online": True})
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
