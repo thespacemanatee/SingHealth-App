1d49f3e7b2c79893aa59a0a736f81e6d096baf95
beazt123 <lohzhixuan@live.com>
2021-03-13 23:23:27 +0800

    Refactored the backend code for new filledAuditForms json schema

--- backend/BTS/auditsEndpoint.py
+++ backend/BTS/auditsEndpoint.py
@@ -3,12 +3,8 @@ from .constants import MAX_NUM_IMAGES_PER_NC
 from flask import request
 from flask_login import login_required
 from pymongo.errors import DuplicateKeyError
+
 # does not give ID to converted form
-
-
-
-
-# TODO: Add form checking in compliance to json schema
 # TODO: Add random integer or smth to the IDs
 def convertToFilledAuditForm(filledAuditFormTemplate):
     filledAuditForm = dict()
@@ -17,10 +13,13 @@ def convertToFilledAuditForm(filledAuditFormTemplate):
 
     # Remove all the questions from the "questions" field
     questions = filledAuditFormTemplate["questions"]
-    answers = []
-    for question in questions:
-        question.pop("question")
-        answers.append(question)
+    answers = {}
+    for category, answerList in questions.items():
+        removedQuestion = []
+        for answer in answerList:
+            answer.pop("question")
+            removedQuestion.append(answer)
+        answers[category] = removedQuestion
 
     filledAuditForm["answers"] = answers
 
@@ -28,22 +27,23 @@ def convertToFilledAuditForm(filledAuditFormTemplate):
 
 def validateFilledAuditForm(filledAuditForm):
     answers = filledAuditForm["answers"]
-    for index, answer in enumerate(answers):
-        if not "answer" in answer.keys():
-            return False, f"Item at index {index} does not have an 'answer' attribute"
+    for category, answerList in answers.items():
+        for index, answer in enumerate(answerList):
+            if not "answer" in answer.keys():
+                return False, f"Item at index {index} does not have an 'answer' attribute"
+            
+            if "images" in answer.keys():
+                if len(answer["images"]) > MAX_NUM_IMAGES_PER_NC:
+                    return False, f"Item at index {index} has > {MAX_NUM_IMAGES_PER_NC} images (Max is {MAX_NUM_IMAGES_PER_NC})"
+
+                numUniqueFilenames = len(set(answer["images"]))
+                numFilenames = len(answer["images"])
+                if numFilenames > numUniqueFilenames:
+                    return False, f"Item at index {index} has duplicate filenames"
+
+            if not answer["answer"] and len(answer["remarks"]) == 0:
+                return False, f"Item at index {index} non-compliant but no remarks were given"
         
-        if "images" in answer.keys():
-            if len(answer["images"]) > MAX_NUM_IMAGES_PER_NC:
-                return False, f"Item at index {index} has > {MAX_NUM_IMAGES_PER_NC} images (Max is {MAX_NUM_IMAGES_PER_NC})"
-
-            numUniqueFilenames = len(list(set(answer["images"])))
-            numFilenames = len(answer["images"])
-            if numFilenames > numUniqueFilenames:
-                return False, f"Item at index {index} has duplicate filenames"
-
-        if not answer["answer"] and len(answer["remarks"]) == 0:
-            return False, f"Item at index {index} non-compliant but no remarks were given"
-    
     return True, "Form is valid and ready for uploading"
 
 def createIDForFilledForm(formTemplate, metadata):
@@ -66,7 +66,9 @@ def processAuditdata(auditData):
     auditForms = auditData["auditForms"]
     auditMetaData["auditChecklists"] = {}
 
-    auditMetaData["_id"] = createIDForAuditMetaData(auditMetaData)
+    metaDataID = createIDForAuditMetaData(auditMetaData)
+    auditMetaData["_id"] = metaDataID
+    print(f"ID of auditMetadata: {metaDataID}")
 
     filledAuditForms = {}
     for formType, formTemplate in auditForms.items():
@@ -74,6 +76,7 @@ def processAuditdata(auditData):
             continue
         filledAuditForm = convertToFilledAuditForm(formTemplate)
         id = createIDForFilledForm(formTemplate, auditMetaData)
+        print(f"ID of {formType}: {id}")
         filledAuditForm["_id"] = id
         filledAuditForms[filledAuditForm["type"]] = filledAuditForm
         auditMetaData["auditChecklists"][filledAuditForm["type"]] = id
@@ -90,7 +93,7 @@ def validateFilledAuditForms(filledAuditForms):
 
 def addAuditsEndpoint(app, mongo):
     @app.route("/audits", methods=['POST'])
-    @login_required
+    # @login_required
     def audits():
         if request.method == 'POST':
             auditData = request.json
