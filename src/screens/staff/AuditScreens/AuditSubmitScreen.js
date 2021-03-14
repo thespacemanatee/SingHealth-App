01233945072e36703db0634e97c8d1dbcad7eca8
Chee Kit <cheekit.chong98@gmail.com>
2021-03-14 12:42:04 +0800

    Fixed CORS policy and added robust error logging

--- src/screens/staff/AuditScreens/AuditSubmitScreen.js
+++ src/screens/staff/AuditScreens/AuditSubmitScreen.js
@@ -140,13 +140,28 @@ const AuditSubmitScreen = ({ navigation }) => {
       .all([axios(post_audit), axios(post_images)])
       .then(
         axios.spread((req1, req2) => {
-          console.log(req1.status, "req1");
-          console.log(req2.status, "req2");
+          console.log(req1.data, "req1");
+          console.log(req2.data, "req2");
         })
       )
       .catch((error) => {
         setError(true);
-        console.error(error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.error(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error("Error", error.message);
+        }
+        console.error(error.config);
       });
 
     setSubmitting(false);
