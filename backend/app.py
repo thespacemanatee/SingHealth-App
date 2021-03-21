ef05231c75af16b5bbdac8660697b197de60d972
beazt123 <lohzhixuan@live.com>
2021-03-22 01:10:06 +0800

    Added new URI for cloud Mongo

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from BTS.auditsEndpoint import addAuditsEndpoint
 from BTS.loginEndpoints import addLoginEndpointsForTenantAndStaff
 from BTS.imagesEndpoint import addImagesEndpoint
 from BTS.auditsEndpoint_wx import addWenXinEndpoints
-from BTS.constants import MONGODB_URI
+from BTS.constants import CLOUD_MONGODB_URI
 from BTS.utils import successMsg, successResponse
 from flask import Flask, request
 from flask_pymongo import PyMongo
@@ -11,7 +11,7 @@ import secrets
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = MONGODB_URI
+app.config["MONGO_URI"] = CLOUD_MONGODB_URI
 app.config["SECRET_KEY"] = secrets.token_urlsafe(nbytes=32)
 mongo = PyMongo(app)
 CORS(app, supports_credentials=True)
@@ -38,14 +38,5 @@ addWenXinEndpoints(app, mongo)
 addAuditsEndpoint(app, mongo)
 addImagesEndpoint(app)
 addLoginEndpointsForTenantAndStaff(app, mongo)
-
-
-# @app.after_request
-# def after_request_func(response):
-#     response.headers["Access-Control-Allow-Origin"] = "http://localhost:19006"
-#     response.headers["Access-Control-Allow-Credentials"] = true
-#     print("after_request is running!", response)
-#     return response
-
 
 app.run(debug=True)
