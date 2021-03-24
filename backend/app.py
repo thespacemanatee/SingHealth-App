b4f484a112051b78a0034cfda2c26d675fc074f7
Chee Kit <cheekit.chong98@gmail.com>
2021-03-24 17:45:41 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -47,4 +47,5 @@ addLoginEndpointsForTenantAndStaff(app, mongo)
 addRecentAuditsEndpoints(app, mongo)
 
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
