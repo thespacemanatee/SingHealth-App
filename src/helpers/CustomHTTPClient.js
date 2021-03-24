96149a8d3c7e9542a0a3aedbb340fc6ffb782958
Chee Kit <cheekit.chong98@gmail.com>
2021-03-24 15:58:42 +0800

    Update CustomHTTPClient.js

--- src/helpers/CustomHTTPClient.js
+++ src/helpers/CustomHTTPClient.js
@@ -5,7 +5,7 @@ export const httpClient = axios.create();
 
 httpClient.defaults.timeout = 10000;
 
-export const endpoint = "https://singhealth-bts-backend.herokuapp.com/";
+export const endpoint = "https://singhealth-backend-bts.herokuapp.com/";
 
 // if (Platform.OS === "android") {
 //   endpoint = "http://10.0.2.2:5000/";
