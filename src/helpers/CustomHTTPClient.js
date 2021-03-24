7b7e692baa07fe7e6778cc43532f78ab80560065
Chee Kit <cheekit.chong98@gmail.com>
2021-03-24 15:20:24 +0800

    Update app.run() to run on heroku

--- src/helpers/CustomHTTPClient.js
+++ src/helpers/CustomHTTPClient.js
@@ -5,11 +5,10 @@ export const httpClient = axios.create();
 
 httpClient.defaults.timeout = 10000;
 
-// eslint-disable-next-line import/no-mutable-exports
-export let endpoint;
+export const endpoint = "https://singhealth-bts-backend.herokuapp.com/";
 
-if (Platform.OS === "android") {
-  endpoint = "http://10.0.2.2:5000/";
-} else {
-  endpoint = "http://localhost:5000/";
-}
+// if (Platform.OS === "android") {
+//   endpoint = "http://10.0.2.2:5000/";
+// } else {
+//   endpoint = "http://localhost:5000/";
+// }
