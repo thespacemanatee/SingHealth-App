f70d5a23142149aa28060f6100fe30c0b8167461
Chee Kit <cheekit.chong98@gmail.com>
2021-03-24 14:10:19 +0800

    Test heroku

---
+++ src/helpers/CustomHTTPClient.js
@@ -0,0 +1,15 @@
+import { Platform } from "react-native";
+import axios from "axios";
+
+export const httpClient = axios.create();
+
+httpClient.defaults.timeout = 10000;
+
+// eslint-disable-next-line import/no-mutable-exports
+export const endpoint = "https://singhealth-bts-backend.herokuapp.com/";
+
+// if (Platform.OS === "android") {
+//   endpoint = "http://10.0.2.2:5000/";
+// } else {
+//   endpoint = "http://localhost:5000/";
+// }
