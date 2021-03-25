92cdc8494e7a17501c0339aacb05eb7424284290
Chee Kit <cheekit.chong98@gmail.com>
2021-03-25 15:44:55 +0800

    install python-dotenv

--- src/helpers/CustomHTTPClient.js
+++ src/helpers/CustomHTTPClient.js
@@ -1,14 +1,16 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
 export const httpClient = axios.create();
 
 httpClient.defaults.timeout = 10000;
 httpClient.defaults.withCredentials = true;
 
-export const endpoint = "https://singhealth-backend-bts.herokuapp.com/";
+// export const endpoint = "https://singhealth-backend-bts.herokuapp.com/";
+export let endpoint;
 
-// if (Platform.OS === "android") {
-//   endpoint = "http://10.0.2.2:5000/";
-// } else {
-//   endpoint = "http://localhost:5000/";
-// }
+if (Platform.OS === "android") {
+  endpoint = "http://10.0.2.2:5000/";
+} else {
+  endpoint = "http://localhost:5000/";
+}
