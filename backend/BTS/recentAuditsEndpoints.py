cd8356e7d9b2d1f755cb044c776d549856c31200
Chee Kit <cheekit.chong98@gmail.com>
2021-03-27 02:20:42 +0800

    Fix indentation

--- backend/BTS/recentAuditsEndpoints.py
+++ backend/BTS/recentAuditsEndpoints.py
@@ -28,21 +28,21 @@ def addRecentAuditsEndpoints(app, mongo):
 
                 return serverResponse(auditsList, 200, "Forms found")
 
-            else:
-                return serverResponse(None, 403, "You do not have access to this as you are not a tenant")
+            # else:
+            #     return serverResponse(None, 403, "You do not have access to this as you are not a tenant")
 
     @app.route("/audits/unrectified/recent/staff/<institutionID>/<int:daysBefore>", methods=['GET'])
     # @login_required
     def unrectified_audits_staff(institutionID, daysBefore):
         if request.method == 'GET':
             # if session["account_type"] == "staff":
-                queryDict = {}
-                queryDict["institutionID"] = institutionID
-                queryDict["rectificationProgress"] = {"$lt": 1}
-                if daysBefore > 0:
-                    queryDict["date"] = {
-                        "$gt": datetime.utcnow() - datetime.timedelta(days=daysBefore)
-                    }
+            queryDict = {}
+            queryDict["institutionID"] = institutionID
+            queryDict["rectificationProgress"] = {"$lt": 1}
+            if daysBefore > 0:
+                queryDict["date"] = {
+                    "$gt": datetime.utcnow() - datetime.timedelta(days=daysBefore)
+                }
 
             audits = mongo.db.audits.find(queryDict)
             auditsList = []
@@ -54,5 +54,5 @@ def addRecentAuditsEndpoints(app, mongo):
 
                 return serverResponse(auditsList, 200, "Forms found")
 
-            else:
-                return serverResponse(None, 403, "You do not have access to this as you are not a staff")
+            # else:
+            #     return serverResponse(None, 403, "You do not have access to this as you are not a staff")
