1ac75c01148a4c04f0d2b13ef1fcc1b6b0a59df5
Xalp <50314159+Xalp@users.noreply.github.com>
2021-03-27 13:36:05 +0800

    ADD: enzyme screen test

--- __tests__/jest-setup.js
+++ __tests__/jest-setup.js
@@ -1,5 +1,9 @@
 import 'react-native-gesture-handler/jestSetup';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+configure({ adapter: new Adapter() });
+
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 jest.mock('react-native-reanimated', () => {
