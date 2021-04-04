9b0bd57c87276e1bb802e6dbf46563413e68861b
Xalp <xalphinions@outlook.com>
2021-04-04 15:19:31 +0800

    ADD: login script added

---
+++ python test/TenantLogin.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from random import choice, randint
+import time
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.get('http://localhost:19006/')
+    driver.find_element_by_xpath('//div[text()="Login"]').click()
+    time.sleep(1)
+    driver.find_element_by_xpath("//input[@type='email']").send_keys("12345678@gmail.com")
+    time.sleep(1)
+    driver.find_element_by_xpath("//input[@type='password']").send_keys("12345678")
+    time.sleep(1)
+    links = driver.find_elements_by_xpath('//div[text()="Login"]')
+    links[-1].click()
+     
+        
