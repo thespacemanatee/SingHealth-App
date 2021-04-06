1b1c3340a207ec5e81b7431eec788c1a7375b149
beazt123 <lohzhixuan@live.com>
2021-04-06 14:29:31 +0800

    Moved the BTS email and app email config here

--- backend/BTS/constants.py
+++ backend/BTS/constants.py
@@ -1,1 +1,13 @@
 MAX_NUM_IMAGES_PER_NC = 3
+
+BTS_EMAIL = "Build.Tech.for.SingHealth@gmail.com"
+
+BTS_APP_CONFIG_EMAIL = dict(
+        DEBUG=True,
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=587,
+        MAIL_USE_TLS=True,
+        MAIL_USE_SSL=False,
+        MAIL_USERNAME=BTS_EMAIL,
+        MAIL_PASSWORD='BTS_admin_1',
+        )
