7bdcd895da5fba9ad86ea3860358d2aaf97346b1
Chee Kit <cheekit.chong98@gmail.com>
2021-04-07 17:21:19 +0800

    Added cross platform Skeleton Loading component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-/* eslint-disable global-require */
-// eslint-disable-next-line import/prefer-default-export
 export { default as LottieView } from "lottie-react-native";
-
 export {
   default as ContentLoader,
-  Facebook,
+  Rect,
+  Circle,
+  List,
 } from "react-content-loader/native";
--- src/index.web.js
+++ src/index.web.js
@@ -1,6 +1,2 @@
-/* eslint-disable global-require */
-// eslint-disable-next-line import/prefer-default-export
-// export const LottieView = require("react-native-web-lottie").default;
 export { default as LottieView } from "react-native-web-lottie";
-
-export { default as ContentLoader, Facebook } from "react-content-loader";
+export { default as ContentLoader, List } from "react-content-loader";
