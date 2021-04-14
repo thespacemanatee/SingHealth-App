002f50185b5af6dfa38c24bf01b2956e8a9b940b
Chee Kit <cheekit.chong98@gmail.com>
2021-04-14 17:07:45 +0800

    Removed unused dependencies and get window width instead of screen

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,7 @@
 import { TransitionPresets } from "@react-navigation/stack";
 import { Dimensions } from "react-native";
 
-export const SCREEN_WIDTH = Dimensions.get("screen").width;
+export const SCREEN_WIDTH = Dimensions.get("window").width;
 export const SCREEN_HEIGHT = Dimensions.get("screen").height;
 export const IMAGE_WIDTH = SCREEN_WIDTH * 0.8;
 export const IMAGE_HEIGHT = (IMAGE_WIDTH / 3) * 4;
