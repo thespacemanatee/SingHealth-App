01addeff50fdc04abd7fd058bebdeb9a845a69bd
beazt123 <lohzhixuan@live.com>
2021-04-17 21:38:50 +0800

    Fixed merge conflicts

--- backend/BTS/constants.py
+++ backend/BTS/constants.py
@@ -3,18 +3,6 @@ MAX_NUM_IMAGES_PER_NC = 20
 BTS_EMAIL = "Build.Tech.for.SingHealth@gmail.com"
 
 BTS_APP_CONFIG_EMAIL = dict(
-<<<<<<< Updated upstream
-    DEBUG=True,
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USE_SSL=False,
-    MAIL_USERNAME=BTS_EMAIL,
-    MAIL_PASSWORD='BTS_admin_1',
-)
-
-SGT_TIMEZONE = "Asia/Singapore"
-=======
         DEBUG=True,
         MAIL_SERVER='smtp.gmail.com',
         MAIL_PORT=587,
@@ -41,4 +29,3 @@ IMAGE_FILETYPES = ['rgb',
 'png',
 'webp',
 'exr']
->>>>>>> Stashed changes
