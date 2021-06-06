ded421f56f4b5342cf8c1da7adaf4f0df2c58697
Chee Kit <cheekit.chong98@gmail.com>
2021-06-06 18:05:58 +0800

    Added filter function and fixed card shadow on web

--- src/components/ui/ShadowCard.js
+++ src/components/ui/ShadowCard.js
@@ -20,11 +20,16 @@ export default ShadowCard;
 
 const themedStyles = StyleService.create({
   shadowContainer: {
-    elevation: 10,
-    shadowOffset: { width: 5, height: 5 },
-    shadowColor: "grey",
-    shadowOpacity: 0.7,
-    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    borderRadius: 10,
     marginBottom: 10,
   },
   shadow: {
