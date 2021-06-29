586801ff8d290f80fabf61e3e7ebe0acac769995
Chee Kit <cheekit.chong98@gmail.com>
2021-06-29 17:19:13 +0800

    Reduce FAB shadow and use clearer error message

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -49,7 +49,10 @@ export const handleErrorResponse = (err, action) => {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    alert("Request timeout", "Check your internet connection.");
+    alert(
+      "Request timeout",
+      "Unable to establish connection to the server. Please try again later."
+    );
   } else {
     // Something happened in setting up the request that triggered an Error
     console.error(err);
